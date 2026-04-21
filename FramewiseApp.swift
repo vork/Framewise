4cbe381e7288159696361d278640a9d64a011d3d
Mark Boss <mark.boss@stability.ai>
2026-04-21 09:20:31 +0200

    Support multiple windows of the same app opened

--- FramewiseApp.swift
+++ FramewiseApp.swift
@@ -5,13 +5,26 @@ struct FramewiseApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "comparison") {
             ContentView()
         }
         .defaultSize(width: 1600, height: 1000)
         .commands {
-            CommandGroup(replacing: .newItem) {}
+            CommandGroup(replacing: .newItem) {
+                NewWindowButton()
+            }
         }
+    }
+}
+
+private struct NewWindowButton: View {
+    @Environment(\.openWindow) private var openWindow
+
+    var body: some View {
+        Button("New Window") {
+            openWindow(id: "comparison")
+        }
+        .keyboardShortcut("n", modifiers: .command)
     }
 }
 
